a47cc7082b4418cc2cf35e33b6c406d3d41603a2
Mykyta Topchii <nikita.topchiy@gmail.com>
2025-08-14 01:29:00 +0300

    Added tests for erc20 repository

--- tests/integration_tests/db/repository_tests.rs
+++ tests/integration_tests/db/repository_tests.rs
@@ -5,6 +5,7 @@ use ethers::types::{H160, H256, U64, U256};
 use logger_playground::{
     db::{
         models::{Erc20TranferEvent, NewErc20TransferEvent},
+        query_filters::{FilterBy, Page},
         repository::EventRepository,
         transfer_repository::ERC20TransferRepo,
     },
@@ -14,7 +15,7 @@ use logger_playground::{
 use r2d2::Pool;
 
 use crate::common::{
-    fill_tables::{BLOCK_NUMBER, FROM, LOG_INDEX, TO, TX_HASH, VALUE},
+    fill_tables::{BLOCK_NUMBER, FROM, LOG_INDEX, TO, TX_HASH, VALUE, fill_db_with_test_data},
     setup_db,
 };
 
@@ -70,4 +71,118 @@ async fn test_store_event() {
             .parse::<f64>()
             .expect("expected value")
     );
+}
+
+#[tokio::test]
+async fn test_get_event_by_tx_hash() {
+    let pool = setup_db();
+
+    fill_db_with_test_data(&mut pool.get().expect("expected conn"));
+
+    let repo = create_erc20_repo(pool.clone());
+    let event = event_message();
+    repo.store_event(event.clone()).await.expect("store event");
+
+    let filter = FilterBy::TxHash(event.tx_hash);
+    let page = Page {
+        limit: Some(10),
+        offset: Some(0),
+    };
+    let events = repo.get_events_by(filter, page).await.expect("get event");
+
+    assert_eq!(events.len(), 1);
+    assert!(
+        events
+            .iter()
+            .any(|v| v.tx_hash.eq(&format!("{:?}", event.tx_hash)))
+    );
+}
+
+#[tokio::test]
+async fn test_get_event_by_from_address() {
+    let pool = setup_db();
+    let repo = create_erc20_repo(pool.clone());
+    let event = event_message();
+    repo.store_event(event.clone()).await.expect("store event");
+
+    let from = format!("{:?}", event.event.from);
+
+    let filter = FilterBy::From(from.clone());
+    let page = Page {
+        limit: Some(10),
+        offset: Some(0),
+    };
+    let events = repo.get_events_by(filter, page).await.expect("get event");
+
+    assert_eq!(events.len(), 1);
+    assert_eq!(events[0].from, from);
+}
+
+#[tokio::test]
+async fn test_get_event_by_to_address() {
+    let pool = setup_db();
+    let repo = create_erc20_repo(pool.clone());
+    let event = event_message();
+    repo.store_event(event.clone()).await.expect("store event");
+
+    // Filter by to address
+    let filter = FilterBy::To(format!("{:?}", event.event.to));
+    let page = Page {
+        limit: Some(10),
+        offset: Some(0),
+    };
+    let events = repo.get_events_by(filter, page).await.expect("get event");
+
+    assert_eq!(events.len(), 1);
+    assert_eq!(events[0].to, format!("{:?}", event.event.to));
+}
+
+#[tokio::test]
+async fn test_get_event_by_block_number() {
+    let pool = setup_db();
+    let repo = create_erc20_repo(pool.clone());
+    let event = event_message();
+    repo.store_event(event.clone()).await.expect("store event");
+
+    // Filter by block number
+    let filter = FilterBy::BlockNumber(event.block_number.as_u64());
+    let page = Page {
+        limit: Some(10),
+        offset: Some(0),
+    };
+    let events = repo.get_events_by(filter, page).await.expect("get event");
+
+    assert_eq!(events.len(), 1);
+    assert_eq!(events[0].block_number, event.block_number.to_string());
+}
+
+#[tokio::test]
+async fn test_get_event_with_pagination() {
+    let pool = setup_db();
+    let repo = create_erc20_repo(pool.clone());
+
+    fill_db_with_test_data(&mut pool.get().expect("expected conn"));
+
+    let filter = FilterBy::Ids(vec![3, 4, 5, 6]);
+    let page = Page {
+        limit: Some(2),
+        offset: Some(0),
+    };
+    let events = repo
+        .get_events_by(filter.clone(), page)
+        .await
+        .expect("get event");
+
+    assert_eq!(events.len(), 2);
+    assert_eq!(events[0].id, 3);
+    assert_eq!(events[1].id, 4);
+
+    let page = Page {
+        limit: Some(2),
+        offset: Some(2),
+    };
+    let events = repo.get_events_by(filter, page).await.expect("get event");
+    assert_eq!(events.len(), 2);
+    assert_eq!(events[0].id, 5);
+    assert_eq!(events[1].id, 6);
 }
